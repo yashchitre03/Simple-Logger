17f321a589f810872a3dc7b46db987238341f95c
yashchitre03 <41328573+yashchitre03@users.noreply.github.com>
2021-05-05 01:51:32 -0500

    Add test_performance.py

---
+++ tests/test_performance.py
@@ -0,0 +1,44 @@
+import time
+from simpleLogger import Log
+
+
+def setup():
+    return open('tests/logs/test.log', 'w').close()
+
+
+def intensive_work(limit: int) -> None:
+    temp = []
+    for i in range(limit):
+        temp.append(i)
+
+    while temp:
+        temp.pop()
+
+
+def test_time():
+    time_original, time_with_logging, impact = [], [], []
+    decorated_fn = Log(profile='clean')(intensive_work)
+    arg = 100000
+
+    for i in (j**2 for j in range(1, 35)):
+        setup()
+
+        start = time.time()
+        for times in range(i):
+            intensive_work(arg)
+        time_original.append(time.time() - start)
+
+        start = time.time()
+        for times in range(i):
+            decorated_fn(arg)
+        time_with_logging.append(time.time() - start)
+
+        try:
+            impact.append((time_original[-1] - time_with_logging[-1]) * 100 / time_with_logging[-1])
+        except ZeroDivisionError as e:
+            impact.append(0)
+
+    print(sum(impact) / len(impact))
+    print(time_original[-1], time_with_logging[-1], impact[-1])
+    print(impact)
+    assert sum(impact) / len(impact) < 150
