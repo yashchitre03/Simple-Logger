abf7b07aa6ad5688aefc79229ca840bea2f78735
yashchitre03 <41328573+yashchitre03@users.noreply.github.com>
2021-05-05 18:30:17 -0500

    Fix packaging issue

--- setup.py
+++ setup.py
@@ -11,17 +11,16 @@ README = (DIR / 'README.md').read_text()
 
 setup(
     name='simpleLogger',
-    packages=find_packages(exclude='tests'),
+    packages=find_packages(exclude=('tests', )),
     version='0.1.0',
     description='A simple logging library for Python that uses decorators to provide logging,'
                 ' exception handling, and type-checking.',
     long_description=README,
     long_description_content_type='text/markdown',
-    url='',
     author='Yash Kiran Chitre',
     author_email='ychitr2@uic.edu',
+    url='https://github.com/yashchitre03',
     license='MIT',
-    include_package_data=True,
     install_requires=['pyyaml', 'psutil'],
     setup_requires=['pytest-runner'],
     tests_require=['pytest', 'matplotlib'],
