48c32d8bd987957c2bf51989de0f94dd3f382af6
yashchitre03 <41328573+yashchitre03@users.noreply.github.com>
2021-05-05 15:14:33 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,24 @@
 from setuptools import find_packages, setup
+import pathlib
+
+DIR = pathlib.Path(__file__).parent
+README = (DIR / 'README.md').read_text()
 
 setup(
     name='simpleLogger',
-    packages=find_packages(exclude=('tests')),
+    packages=find_packages(exclude='tests'),
     version='0.1.0',
-    description='My first Python library',
+    description='A simple logging library for Python that uses decorators to provide logging,'
+                ' exception handling, and type-checking.',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    url='',
     author='Yash Kiran Chitre',
+    author_email='ychitr2@uic.edu',
     license='MIT',
-    install_requires=[],
+    include_package_data=True,
+    install_requires=['pyyaml', 'psutil'],
     setup_requires=['pytest-runner'],
-    tests_require=['pytest'],
+    tests_require=['pytest', 'matplotlib'],
     test_suite='tests',
 )
