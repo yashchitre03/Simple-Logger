191052f1aae0b356ca1a2bad661eaba831ff78c3
yashchitre03 <41328573+yashchitre03@users.noreply.github.com>
2021-05-04 23:55:52 -0500

    Add test_working.py

--- tests/test_working.py
+++ tests/test_working.py
@@ -9,11 +9,22 @@ def setup():
     return open('tests/logs/test.log', 'w').close()
 
 
+@Log(profile='clean')
+def dummy(x, y):
+    return x + y
+
+
+def test_config_path(setup):
+    assert Log.CONFIG_FILE is not None
+
+
 @pytest.mark.parametrize("a, b, c", [(1, 2, 3), (5, 5, 10), (-50, 7, -43)])
 def test_basic(setup, a, b, c):
+    assert dummy(a, b) == c
 
-    @Log(profile='clean')
-    def basic_sample(x, y):
-        return x + y
 
-    assert basic_sample(a, b) == c
+def test_logs(setup):
+    dummy(10, 20)
+    with open('tests/logs/test.log') as f:
+        content = f.read()
+        assert '[INFO]' in content
